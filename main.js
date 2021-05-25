b9fa6002d7a61e54eaf1ac283e74580504c5806b
pratyushgguptaa <sonalitiwaridjt@gmail.com>
2021-05-25 16:53:45 +0530

    finally final

--- main.js
+++ main.js
@@ -1,62 +1,59 @@
 import './style.css'
-
 import * as THREE from 'three'
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-const scene = new THREE.Scene()
+// imported everything above
 
+//creating instances of required classes
+const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
+const controls = new OrbitControls(camera, renderer.domElement)
 
+//setting rendered properties and camera position
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 camera.position.setX(-3)
 
+//finally render into the scene
 renderer.render(scene, camera)
 
-// Lights
+//setting a background
+scene.background = new THREE.TextureLoader().load('background.jpg')
 
+//light up the scene
 const pointLight = new THREE.PointLight(0xffffff)
+//these really light up the scene
 pointLight.position.set(15, 15, 15)
-
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
-//scene.add(pointLight)
-const controls = new OrbitControls(camera, renderer.domElement)
 
-const spaceTexture = new THREE.TextureLoader().load('background.jpg')
-scene.background = spaceTexture
-
-// Moon
-const moonTexture = new THREE.TextureLoader().load('earth2.jpg')
-const normalTexture = new THREE.TextureLoader().load('normal4.jfif')
-
+//adding texture to earth
+const earthTexture = new THREE.TextureLoader().load('earth2.jpg')
+const texture = new THREE.TextureLoader().load('normal4.jfif')
+//creating earth
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(13, 32, 32),
   new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
+    map: earthTexture,
+    normalMap: texture,
   })
 )
 
 scene.add(earth)
 
 function animate() {
-  requestAnimationFrame(animate)
-
   earth.rotation.y += 0.002
-
+  requestAnimationFrame(animate)
   controls.update()
-
+  //keep updating the scene
   renderer.render(scene, camera)
 }
 
